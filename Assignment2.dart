10b62d93b416d0648c5c55a32050cff2267e98d9
Destiny Oladapo Adu <aduoladapo9@gmail.com>
2025-07-24 08:06:52 +0100

    Second Dart Lesson Assignment.

---
+++ Assignment2.dart
@@ -0,0 +1,15 @@
+void main(){
+
+  // QUESTION 1: SINGLE QUOTES, DOUBLE QUOTES AND ESCAPE CHARACTERS
+  print("Hello I am \"John Doe\"");
+  print('Hello I am "John Doe"');
+
+  print('Hello I\'m "John Doe"');
+  print("Hello I'm \"John Doe\"");
+
+
+
+  // QUESTION 2: DECLARING CONSTANT VARIABLES
+  const double pi = 3.142;
+  const int numOfDaysInWeek = 7;
+}
